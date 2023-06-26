c00dd021d9f97786929c4ddd32eb0392b5797f34
SwopnilShrestha02 <Swopnilrajshrestha@gmail.com>
2023-06-26 23:42:23 +0545

    increment

---
+++ react_redux/src/store/index.js
@@ -0,0 +1,13 @@
+import {createStore} from 'redux';
+
+const reducerFn = (state={counter: 0},action) =>{
+
+    if (action.type==="INC"){
+        return {counter: state.counter+1}
+    }
+    return state;
+}
+
+const store=createStore(reducerFn)
+
+export default store;
